e8c5b1a681c07a7688e9bc2d55184a5c5669ec8a
skim <skim@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-02 04:45:53 +0000

    Handy script to rebake all the GUI templates in the system.
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@23 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Scripts/rebake_templates.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Rebake all GUI templates.
+ */
+
+if ((php_sapi_name() !== 'cli')) {
+    echo "This script can only be run from the command line.\n";
+    exit();
+}
+
+$rootdir = $argv[1];
+if (is_dir($rootdir) === FALSE) {
+    echo "Please specify the root directory.\n";
+    exit();
+}
+
+
+// Change to Product Root directory.
+chdir($rootdir);
+
+include_once 'Libs/FileSystem/FileSystem.inc';
+include_once 'data/init.inc';
+
+// Clean up existing templates.
+$templateDirs = FileSystem::findDirectories($rootdir.'/data', 'Templates');
+foreach ($templateDirs as $templateDirPath) {
+    FileSystem::clearDirectory($templateDirPath);
+}
+
+// Re-run GUI installation function.
+include_once $rootdir.'/Systems/GUI/GUISystem.inc';
+$guiSys = new GUISystem();
+$guiSys->bakeTemplates();
+
+?>
