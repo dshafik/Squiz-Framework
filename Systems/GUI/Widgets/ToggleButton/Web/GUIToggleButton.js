e285b49b84c7af19de78269b8dad615b3ecbe145
bpearson <bpearson@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-05 06:01:37 +0000

    Initial commit of the Toggle Button widget, which provides a binary option (usually Yes/No).
    Widget Settings allow to set:
    1. 'options' which allows you to set two values (one for Yes and one for No)
    2. 'default' the default value of the field
    3. 'dataprovider' an action to set the current value of this widget.
    
    
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@47 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Systems/GUI/Widgets/ToggleButton/Web/GUIToggleButton.js
@@ -0,0 +1,90 @@
+function GUIToggleButton(id, settings) {
+    this.id = id;
+	this.settings = settings;
+
+	var widgetElement = dfx.getId(id);
+	var selectedElement = dfx.getClass('selected', widgetElement)[0];
+	var currentClass = selectedElement.className.split(' ');
+
+	this.currValue = (dfx.inArray('yes', currentClass) === true) ? true : false;
+
+	GUI.addWidgetEvent(this, 'toggleOn');
+	GUI.addWidgetEvent(this, 'toggleOff');
+
+	this.init();
+}
+
+GUIToggleButton.prototype = {
+	init: function() {
+
+		var self = this;
+		dfx.addEvent(dfx.getId(self.id), 'click', function() {
+
+				if (self.currentValue === true) {
+					// move to false
+					self.setValue(false);
+				} else {
+					// move to true
+					self.setValue(true);
+				}
+				// when the trasition finishes,
+					// fire event
+					//self.fireToggleOnCallbacks();
+		});
+
+    }, 
+
+	getValue: function() {
+		var self = this;
+		return self.currentValue;
+	},
+
+	setValue: function(value) {
+		var self = this;
+		if (value !== self.currentValue) {
+			// Only do something if the value is different from the current value
+			self.currentValue = value;
+			if (value === true) {
+				// move to true
+				self.switchOn();
+			} else {
+				// move to false
+				self.switchOff();
+			}
+		}
+	},
+
+	switchOff: function() {
+		var sec = 300; 
+        var self     = this;
+		var widgetElement = dfx.getId(self.id);
+		var glowMask = dfx.getClass('toggle', widgetElement)[0];
+        dfx.animate(glowMask, {left: 50}, sec, function() {
+
+            var optYes = dfx.getClass('yes', widgetElement)[0];
+            dfx.removeClass(optYes, 'selected');
+            var optNo = dfx.getClass('no', widgetElement)[0];
+            dfx.addClass(optNo, 'selected');
+
+			// finish, fire event
+			self.fireToggleOffCallbacks();
+        });
+	},
+
+	switchOn: function() {
+		var sec = 300; 
+        var self     = this;
+		var widgetElement = dfx.getId(self.id);
+		var glowMask = dfx.getClass('toggle', widgetElement)[0];
+        dfx.animate(glowMask, {left: 6}, sec, function() {
+
+            var optYes = dfx.getClass('yes', widgetElement)[0];
+            dfx.addClass(optYes, 'selected');
+            var optNo = dfx.getClass('no', widgetElement)[0];
+            dfx.removeClass(optNo, 'selected');
+
+			// finish, fire event
+			self.fireToggleOnCallbacks();
+        });
+	}
+}
