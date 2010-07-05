a85e0a2e24037f9c18d148dbf987b43ca28af801
skim <skim@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-05 03:27:26 +0000

    General event handling code added to GUI class.
    This function is intended to help handling widget's custom events. For example, the caller can use it like:
    
    GUI.addWidgetEvent(this, 'screenLoadStarts');
    
    in the constructor. It will add addScreenLoadStartsCallback(cb), fireScreenLoadStartsCallbacks() and removeAllScreenLoadStartsCallbacks() functions to the object, so that each widget doesn't have to handle their own events.
    
    
    
    
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@41 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- Systems/GUI/Web/GUI.js
+++ Systems/GUI/Web/GUI.js
@@ -27,4 +27,51 @@ var GUI = new function()
 
     };
 
+    /**
+     * Add event handler functions to the passed object.
+     *
+     * The three event handling function will be added to the object
+     * passed in.
+     *
+     * @param {Object} obj Widget object to add events..
+     * @param {String} eventName Custom event name to use.
+     * @return {Void}
+     */
+    this.addWidgetEvent = function(obj, eventName) {
+        var self = this;
+
+        if (!obj.__eventCallbacks) {
+            obj.__eventCallbacks = {};
+        }
+
+        if (!obj.__eventCallbacks[eventName]) {
+            obj.__eventCallbacks[eventName] = [];
+        } else {
+            return;
+        }
+
+        var addEvtid = 'add' + dfx.ucFirst(eventName) + 'Callback';
+        obj[addEvtid] = function(callback) {
+            if (typeof callback === 'function') {
+                obj.__eventCallbacks[eventName].push(callback);
+            }
+        };
+
+        var fireEvtid = 'fire' + dfx.ucFirst(eventName) + 'Callbacks';
+        obj[fireEvtid] = function() {
+            if (obj.__eventCallbacks) {
+                var len = obj.__eventCallbacks[eventName].length;
+                for (var i = 0; i < len; i++) {
+                    obj.__eventCallbacks[eventName][i].call(obj);
+                }
+            }
+        };
+
+        var rmEvtid = 'removeAll' + dfx.ucFirst(eventName) + 'Callbacks';
+        obj[rmEvtid] = function() {
+            obj.__eventCallbacks[eventName] = [];
+        };
+
+    };
+
 };
