99175c8b47895f378daa3cc59d99cd0b40226f17
skim <skim@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-14 23:29:42 +0000

    Some basic structure for GUIScreenSwitcher.js
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@106 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- Systems/GUI/Widgets/ScreenSwitcher/Web/GUIScreenSwitcher.js
+++ Systems/GUI/Widgets/ScreenSwitcher/Web/GUIScreenSwitcher.js
@@ -1,4 +1,52 @@
-function GUIScreenSwitcher(id, setting) {}
+function GUIScreenSwitcher(id, settings) {
+    this.id       = id;
+    this.settings = settings;
+    this.elem       = dfx.getId(this.id);
+    this.targetid   = this.settings.target;
+    this.targetElem = dfx.getId(this.targetid);
+
+    this.currScreenid = null;
+
+    /**
+     * screenLoadStarts event.
+     * screenLoadFinished event.
+     */
+    GUI.addWidgetEvent(this, 'screenLoadStarts');
+    GUI.addWidgetEvent(this, 'screenLoadFinished');
+    //this.addScreenLoadStartsCallback(function() {alert('hi');});
+    //this.fireScreenLoadStartsCallbacks();
+
+    this.init();
+}
 
 GUIScreenSwitcher.prototype = {
+
+    init: function()
+    {
+        var self = this;
+        dfx.foreach(this.settings.screens, function(idx) {
+            var screenid = self.id + '-' + self.settings.screens[idx].id;
+            dfx.addEvent(dfx.getId(screenid), 'click', function(evt) {
+                self.loadScreen(screenid);
+            });
+
+            return true;
+        });
+
+    },
+
+    loadScreen: function(id)
+    {
+        var self   = this;
+        var params = {
+            systemName: 'Cron',
+            screenId:   'CronScreen'
+        };
+
+        sfapi.get('GUIScreenSwitcher', 'getScreenContents', params, function(data) {
+            dfx.setHtml(self.targetElem, data.result);
+            console.info('loadScreen: ', data);
+        });
+    }
+
 }
