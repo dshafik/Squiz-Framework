dc1152c0d7b7bb4c42fa375ff81e51c62e3b34dc
bpearson <bpearson@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-19 05:20:49 +0000

    Using the ANSI code (\033[[COL]G) instead of spaces for indentation
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@122 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- Scripts/clean.php
+++ Scripts/clean.php
@@ -32,33 +32,33 @@ $rootdir = realpath($rootdir);
 // Change to mysource mini root directory.
 chdir($rootdir);
 
-echo '1. Removing files from the data directory           ';
+echo '1. Removing files from the data directory';
 system('rm -rf '.$rootdir.'/data/* >> /dev/null 2>&1');
 echoDone();
 
-echo '2. Cleaning out the web directory                   ';
+echo '2. Cleaning out the web directory';
 system('rm -rf '.$rootdir.'/web/* >> /dev/null 2>&1');
 echoDone();
 
-echo '3. Cleaning DAL QueryStore and Oven directories     ';
+echo '3. Cleaning DAL QueryStore and Oven directories';
 system('rm -rf '.$rootdir.'/DAL/QueryStore/* >> /dev/null 2>&1');
 system('rm -rf '.$rootdir.'/DAL/Oven/* >> /dev/null 2>&1');
 echoDone();
 
-echo '4. Cleaning Channels Oven directory                 ';
+echo '4. Cleaning Channels Oven directory';
 system('rm -rf '.$rootdir.'/Channels/Oven/* >> /dev/null 2>&1');
 echoDone();
 
-echo '5. Clean up ._ files from OS X                      ';
+echo '5. Clean up ._ files from OS X';
 system('find '.$rootdir.' -name "._*" -exec rm {} \; >> /dev/null 2>&1');
 echoDone();
 
-echo '6. Fix file system permissions for broken systems   ';
+echo '6. Fix file system permissions for broken systems';
 system('chown -R root:root '.$rootdir);
 system('chmod -R 755 '.$rootdir);
 echoDone();
 
-echo '7. Re-creating database                             ';
+echo '7. Re-creating database';
 include_once 'DAL/DALConf.inc';
 
 $dbName     = $conf['DSN'];
@@ -68,7 +68,7 @@ system('dropdb -U postgres '.$dbName);
 system('createdb -U postgres '.$dbName.' -O '.$dbUserName.' -E SQL_ASCII');
 echoDone();
 
-echo '8. Granting database privileges                     ';
+echo '8. Granting database privileges';
 file_put_contents('./grant_privileges.db', 'GRANT ALL PRIVILEGES ON DATABASE '.$dbName.' to '.$dbUserName.';');
 system('createlang -U postgres plpgsql '.$dbName.' >> /dev/null 2>&1');
 system('psql -U postgres < ./grant_privileges.db >> /dev/null 2>&1');
@@ -86,34 +86,34 @@ require_once $rootdir.'/Libs/FileSystem/FileSystem.inc';
 $systemNames = Install::getInstallOptions(FALSE, FALSE);
 
 // Clean up data and oven dir before full install.
-echo '9. Clean data & oven dir.                           ';
+echo '9. Clean data & oven dir';
 system('rm -rf ./data/* >> /dev/null 2>&1');
 system('rm -rf '.$rootdir.'/DAL/QueryStore/* >> /dev/null 2>&1');
 system('rm -rf '.$rootdir.'/DAL/Oven/* >> /dev/null 2>&1');
 system('rm -rf '.$rootdir.'/Channels/Oven/* >> /dev/null 2>&1');
 echoDone();
 
-echo '10. Preparing data directories.                     ';
+echo '10. Preparing data directories';
 Install::prepareDataDir($rootdir, $url);
 echoDone();
 
-echo '11. Rebaking Oven                                   ';
+echo '11. Rebaking Oven';
 Install::rebakeOven($rootdir, $url);
 echoDone();
 
-echo '12. Installing queries                              ';
+echo '12. Installing queries';
 Install::installQueries($systemNames);
 echoDone();
 
-echo '13. Installing SQL functions                        ';
+echo '13. Installing SQL functions';
 Install::installSqlFunctions($systemNames);
 echoDone();
 
-echo '14. Running system install methods                  ';
+echo '14. Running system install methods';
 Install::runSystemInstallMethods($systemNames);
 echoDone();
 
-echo '15. Copying web files                               ';
+echo '15. Copying web files';
 Install::copyWebFiles();
 echoDone();
 
@@ -121,7 +121,7 @@ require_once $rootdir.'/data/init.inc';
 require_once $rootdir.'/Channels/Channels.inc';
 require_once $rootdir.'/Systems/BaseSystem.inc';
 
-echo '16. Give error_log 777 permission for developers    ';
+echo '16. Give error_log 777 permission for developers';
 if (file_exists($rootdir.'/error_log') === FALSE) {
     file_put_contents($rootdir.'/error_log', '');
 }//end if
@@ -176,7 +176,7 @@ function getSelectedSystems($rootDir, $system)
  */
 function echoDone()
 {
-    echo exec('echo -n "[ " ; echo -en "\033[0;32mDone" ; tput sgr0 ; echo " ]"')."\n";
+    echo exec('echo -en "\033[54G"; echo -n "[ " ; echo -en "\033[0;32mDone" ; tput sgr0 ; echo " ]"')."\n";
 
 }//end echoDone()
 
