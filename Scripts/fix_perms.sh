d74799cf34031ec80056b1f21f1ec0100c7cd315
skim <skim@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-07-21 05:22:44 +0000

    Add fix_perms.sh script to Scripts folder
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@132 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Scripts/fix_perms.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Where are we running?
+self=`readlink -f $0`
+mydir=`dirname $self`
+basedir=`dirname $mydir`
+
+# Fix data and web directory ownership and permissions.
+dirs="$basedir/data $basedir/web"
+for dir in $dirs; do
+    if [ -d "$dir" ] ; then
+        find $dir ! -user www-data -exec chown www-data:www-data {} \;
+        find $dir -type d ! -perm 0755 -exec chmod 755 {} \;
+        find $dir -type f ! -perm 0644 -exec chmod 644 {} \;
+    fi
+done
