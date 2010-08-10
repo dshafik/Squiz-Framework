40b17c1622e0ba7838dd7d10d6093e7f92b497c8
dsherwood <dsherwood@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-08-10 00:39:53 +0000

    Added the license headers
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@203 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 /**
  * MySource 4 index.php.
  *
- * @version    4.0.0
- * @package    MySource4
- * @subpackage MySource4
- * @author     Squiz Pty Ltd <mysource4@squiz.net>
- * @copyright  2006-2007 Squiz Pty Ltd (ABN 77 084 670 600)
- * @license    http://matrix.squiz.net/licence Squiz.Net Open Source Licence
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program as the file license.txt. If not, see
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
+ *
+ * @package    Framework
+ * @subpackage Core
+ * @author     Squiz Pty Ltd <products@squiz.net>
+ * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
  */
 
 // Set this to TRUE to generate SVG files of channel execution paths.
