66b6e9a7e02400a15c77b3fa7de4c4b160ccb008
sdanis <sdanis@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-08-12 01:40:26 +0000

    Fixed widgets, most of them should work with the new GUI system. Removed old methods from GUIActions
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@216 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Systems/GUI/Widgets/ScreenSettings/Web/GUIScreenSettings.js
@@ -0,0 +1,62 @@
+function GUIScreenSettings(id, settings)
+{
+    this.id       = id;
+    this.settings = settings;
+
+    this.className = 'GUIScreenSettings';
+
+    console.info(this.settings);
+}
+
+GUIScreenSettings.prototype = {
+
+    getValue: function()
+    {
+        var items = dfx.getClass(this.className + '-item', dfx.getId(this.id));
+        var value = {};
+        var self  = this;
+        dfx.foreach(items, function(i) {
+            var item         = items[i];
+            var widgetValues = self.getWidgetValues(item);
+            dfx.foreach(widgetValues, function(widgetid) {
+                value[widgetid] = widgetValues[widgetid];
+            });
+        });
+
+        return value;
+
+    },
+
+    getWidgetValues: function(itemElem)
+    {
+        // Find all the elements with ids.
+        var elems   = dfx.find(itemElem, '[id]');
+        var ln      = elems.length;
+        var values  = {};
+
+        if (ln === 0) {
+            return null;
+        }
+
+        for (var i = 0; i < ln; i++) {
+            var elem = elems[i];
+            var id   = elem.getAttribute('id');
+            if (!id) {
+                continue;
+            }
+
+            // Check if id is a valid widget id.
+            var widget = GUI.getWidget(id);
+            if (!widget) {
+                continue;
+            }
+
+            // TODO: Should we only return top level widgets?
+            values[id] = widget.getValue();
+        }//end for
+
+        return values;
+
+    }
+
+};
