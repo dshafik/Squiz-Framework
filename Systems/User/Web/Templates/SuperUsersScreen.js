d255c8870d3a61dcdeefcaa8ec70de7ac6623e51
sdanis <sdanis@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-08-31 06:02:48 +0000

    Added AssetPicker to Super Users screen. Removed test code
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@348 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Systems/User/Web/Templates/SuperUsersScreen.js
@@ -0,0 +1,39 @@
+/**
+ * JS Class for the SuperUsers Screen.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program as the file license.txt. If not, see
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
+ *
+ * @package    Framework
+ * @subpackage User
+ * @author     Squiz Pty Ltd <products@squiz.net>
+ * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
+ */
+
+ var SuperUsersScreen = new function()
+{
+    this.addUser = function() {
+        var options = {
+            modal: true
+        };
+
+        // TODO: Use project asset etc..
+        var templateSettings = {
+            rootNode: 2
+        };
+
+        GUI.loadTemplate('GUIAssetPicker', 'GUIAssetPicker.tpl', templateSettings, null, options);
+    }
+
+};
