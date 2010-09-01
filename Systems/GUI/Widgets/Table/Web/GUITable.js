91f2eeb4c78bd7c6500a9137729fd4eb387cce83
sdanis <sdanis@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-09-01 05:50:33 +0000

    Added generateRows API method to GUITable (both PHP and JS). When new rows need to be added to the table generateRows should be called. This method will use the API method specified by 'rowGenerator' setting to construct the required table row HTML.
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@357 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- Systems/GUI/Widgets/Table/Web/GUITable.js
+++ Systems/GUI/Widgets/Table/Web/GUITable.js
@@ -21,11 +21,17 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
  */
 
-function GUITable(id, settings) {
+function GUITable(id, settings)
+{
     this.id       = id;
     this.settings = settings;
 
+    this.elem = dfx.getId(this.id);
+
+    this._loadingRow = null;
+
     this.init();
+
 }
 
 GUITable.prototype = {
@@ -57,12 +63,10 @@ GUITable.prototype = {
                 }
             });
         }
-    },
-
-    _columnClicked: function(col)
-    {
 
     },
+
+    _columnClicked: function(col) {},
 
     _rowClicked: function(row)
     {
@@ -76,6 +80,97 @@ GUITable.prototype = {
         }
 
         dfx.addClass(row, 'selected');
+
+    },
+
+
+    /**
+     * Generates a new row using a channel and specified row data.
+     *
+     * This method should be used for complex table rows (e.g. rows with widgets),
+     * the channel specified must return array of column contents.
+     *
+     * @param {object}   rowsData Data for the columns.
+     * @param {function} callback The function to call once the row is added.
+     * @param {string}   system   The name of the system that action belongs to. Optional.
+     * @param {string}   action   The name of the action to call. Optional.
+     *
+     * @return {void}
+     */
+    generateRows: function(rowsData, callback, system, action)
+    {
+        var channel = this.settings.rowGenerator;
+        if (system && action) {
+            channel = {
+                system: system,
+                action: action
+            };
+        }
+
+        var tbody = dfx.getTag('tbody', this.elem)[0];
+
+        if (!this._loadingRow) {
+            this._loadingRow = document.createElement('tr');
+            dfx.addClass(this._loadingRow, 'loadingRow');
+
+            var colCount = 0;
+            dfx.foreach(this.settings.columns, function() {
+                colCount++;
+            });
+
+            dfx.setHtml(this._loadingRow, '<td colspan="' + colCount + '"><img src="' + GUI.getWidgetURL('GUI/Table') + '/ajax-loader.gif" /></td>');
+        }
+
+        tbody.appendChild(this._loadingRow);
+
+        var params = {
+            rowsData: dfx.jsonEncode(rowsData),
+            channel: dfx.jsonEncode(channel),
+            settings: dfx.jsonEncode(this.settings)
+        };
+
+        var self = this;
+        GUI.sendRequest('GUITable', 'generateRows', params, function(data) {
+            var rowHTML = data.result;
+            var tmp     = document.createElement('div');
+            dfx.setStyle(tmp, 'display', 'none');
+
+            // Add the tmp div to DOM so that any JS script retrieved will work with
+            // dfx.getId() etc.
+            document.body.appendChild(tmp);
+
+            dfx.setHtml(tmp, rowHTML);
+
+            // Remove loading row.
+            dfx.remove(self._loadingRow);
+
+            // Get the last row.
+            var currentRowCount = tbody.childNodes.length;
+
+            var rows = [];
+            while (tmp.firstChild) {
+                currentRowCount++;
+
+                rows.push(tmp.firstChild);
+                if ((currentRowCount % 2) === 0) {
+                    // Even.
+                    dfx.removeClass(tmp.firstChild, 'rowOdd');
+                    dfx.addClass(tmp.firstChild, 'rowEven');
+                } else {
+                    // Odd.
+                    dfx.removeClass(tmp.firstChild, 'rowEven');
+                    dfx.addClass(tmp.firstChild, 'rowOdd');
+                }
+
+                tbody.appendChild(tmp.firstChild);
+            }
+
+            dfx.remove(tmp);
+
+            if (callback) {
+                callback.call(self, rows);
+            }
+        });
 
     },
 
@@ -111,9 +206,9 @@ GUITable.prototype = {
 
             var colIndex = colid.replace(this.id + '-' + rowid + '-', '');
 
-            var widgetValue = colWidget.getValue();
+            var widgetValue        = colWidget.getValue();
             value[rowid][colIndex] = widgetValue;
-        }
+        }//end for
 
         return value;
 
