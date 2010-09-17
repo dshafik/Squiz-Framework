20d01d23198aa33ae6f66d80537b1a4a37e35f65
skim <skim@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-09-17 01:07:45 +0000

    TextArea widget added.
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@484 1b08cf3a-d8dc-418b-9366-0d0b919357ef

---
+++ Systems/GUI/Widgets/TextArea/Web/GUITextArea.js
@@ -0,0 +1,92 @@
+/**
+ * JS Class for the Text Box Widget.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program as the file license.txt. If not, see
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
+ *
+ * @package    Framework
+ * @subpackage GUI
+ * @author     Squiz Pty Ltd <products@squiz.net>
+ * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
+ */
+
+function GUITextArea(id, settings)
+{
+    this.id           = id;
+    this.settings     = settings;
+    var widgetElement = dfx.getId(self.id);
+    this.textArea     = dfx.getClass('textarea', widgetElement)[0];
+
+    this.init();
+
+}
+
+GUITextArea.prototype = {
+    init: function()
+    {
+        var self          = this;
+        var widgetElement = dfx.getId(self.id);
+        var textArea      = dfx.getClass('textarea', widgetElement)[0];
+
+        dfx.addEvent(textArea, 'blur', function() {
+            dfx.removeClass(textArea, 'selected');
+        });
+
+        dfx.addEvent(textArea, 'focus', function() {
+            dfx.addClass(textArea, 'selected');
+        });
+
+        dfx.addEvent(textArea, 'keypress', function() {
+            if (value !== this.value) {
+                // If the value of the box has changed, set it modified.
+                value = this.value;
+                GUI.setModified(self, true);
+            }
+        });
+
+    },
+
+    getValue: function()
+    {
+        var widgetElement = dfx.getId(this.id);
+        var textArea      = dfx.getClass('textarea', widgetElement)[0];
+        return textArea.value;
+
+    },
+
+    setValue: function(value)
+    {
+        var widgetElement = dfx.getId(this.id);
+        var textArea      = dfx.getClass('textarea', widgetElement)[0];
+        if (value !== textArea.value) {
+            // Only do something if the value is different from the current value.
+            textArea.value = value;
+            GUI.setModified(this, true);
+        }
+
+    },
+
+    focus: function()
+    {
+        this.textArea.focus();
+
+    },
+
+    select: function()
+    {
+        this.textArea.select();
+
+    }
+
+}
