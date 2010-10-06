442655070c4102e74fc71a580f289b983b3a4af2
lwright <lwright@1b08cf3a-d8dc-418b-9366-0d0b919357ef>
2010-10-06 01:13:30 +0000

    First useful commit of Date Field GUI widget.
    The widget is essentially three Select widgets that control the day, month and year of the date to be selected. getValue() returns the date in YYYY-MM-DD format; or getDateObject() returns a JavaScript Date object.
    
    Settings:
    - initialDate - the date to be initially loaded into the widget. Preferred YYYY-MM-DD format, however programmatic creations of the widget can also send in a UNIX timestamp. Defaults to today's server date.
    - yearRange - the range of years to show in the drop-down for year; consisting of min and max elements; defaults to {min: 1900, max: 2100}. Values of the format "+5" or "-5" are accepted as relative values to the current year - so for instance, if you wanted to show only this year and the next four (for scheduling, say), you can use {min: '+0', max: '+4'}. Note that it must be "+0" or "-0"; the presence of the plus or minus sign indicates the presence of a relative date (plain "0" will not work).
    - inline - if FALSE, the drop-downs will be placed inside a DIV; if TRUE, inlined and placed inside a SPAN so it can still be accessed. Default FALSE.
    
    Events:
    - change - When any of the date fields are changed. Sends the new date value in the callback arguments. (Any of the Select widgets that make up the Date Field widget can also be targeted by their change events, if absolutely necessary.)
    
    Notes:
    - The code will automatically validate dates as the drop-down values are changed, to guard against invalid dates at the end of the month - it will disable the relevant days and set the field to the last day in the month. For instance, If you change a "31 May" date to April, day "31" will be disabled in the drop-down, and the field will be set to "30 April".
    
    
    git-svn-id: http://public-cvs.squiz.net/svn/squiz-framework@603 1b08cf3a-d8dc-418b-9366-0d0b919357ef

--- Systems/GUI/Widgets/DateField/Web/GUIDateField.js
+++ Systems/GUI/Widgets/DateField/Web/GUIDateField.js
@@ -26,8 +26,17 @@ function GUIDateField(id, settings)
 	this.id       = id;
 	this.settings = settings;
 
-	this.init();
+	this._currentValue = this.settings.initialDate;
+	this._dayWidget    = GUI.getWidget(this.id + '-day');
+	this._monthWidget  = GUI.getWidget(this.id + '-month');
+	this._yearWidget   = GUI.getWidget(this.id + '-year');
 
+	this.changeEnabled = false;
+
+	// Add a widget for the date changing.
+	GUI.addWidgetEvent(this, 'change');
+
+	this.init();
 }
 
 GUIDateField.prototype = {
@@ -36,5 +45,145 @@ GUIDateField.prototype = {
 	 */
 	init: function()
 	{
+	    var self = this;
+	    this.validateDate();
+
+	    this._dayWidget.addChangeCallback(function() {
+            if (self.changeEnabled === true) {
+                self.dateChanged.call(self);
+            }
+	    });
+
+	    this._monthWidget.addChangeCallback(function() {
+            if (self.changeEnabled === true) {
+                self.dateChanged.call(self);
+	        }
+	    });
+
+	    this._yearWidget.addChangeCallback(function() {
+            if (self.changeEnabled === true) {
+                self.dateChanged.call(self);
+            }
+	    });
+
+	    this.changeEnabled = true;
+	},
+
+	validateDate: function()
+	{
+	    var day   = this._dayWidget.getValue()[0];
+	    var month = this._monthWidget.getValue()[0];
+	    var year  = this._yearWidget.getValue()[0];
+
+	    var leapYear = false;
+	    if (year % 100 === 0) {
+	        if (year % 400 === 0) {
+	            leapYear = true;
+	        }
+	    } else {
+	        if (year % 4 === 0) {
+	            leapYear = true;
+	        }
+	    }//end if
+
+	    // Calculate how many days there should be in the month.
+	    var maxDay = 31;
+	    if (month === '02') {
+	        if (leapYear === true) {
+	            maxDay = 29;
+	        } else {
+	            maxDay = 28;
+	        }
+	    } else {
+	        var monthsWith30Days = ['04', '06', '09', '11'];
+	        if (monthsWith30Days.inArray(month) === true) {
+	            maxDay = 30;
+	        }
+	    }//end if
+
+	    // If a month/year change has caused the day to fall out of range,
+	    // change it. Don't fire an additional dateChanged event for this.
+	    if (day > maxDay) {
+	        var oldCE = this.changeEnabled;
+	        this.changeEnabled = false;
+	        this._dayWidget.setValue([maxDay.toString()]);
+	        this.changeEnabled = oldCE;
+	    }
+
+	    // Disable any days in the month that are not valid for this month/year
+	    // selection.
+	    var daySelect = dfx.getId(this._dayWidget.id);
+	    for (var i = 0; i < daySelect.options.length; i++) {
+	        var option = daySelect.options[i];
+	        if (parseInt(option.value, 10) > maxDay) {
+	            option.disabled = true;
+	        } else {
+	            option.disabled = false;
+	        }
+	    }
+	},
+
+	dateChanged: function()
+	{
+        this.validateDate();
+        var newValue = this.getValue();
+
+        // Invalid date. Do not change our current value.
+        if (newValue === null) {
+            newValue = this.currentValue;
+        }
+
+        if (this.currentValue !== newValue) {
+            this.currentValue = newValue;
+            this.fireChangeCallbacks(newValue);
+        }
+	},
+
+	getValue: function()
+	{
+	    var ok = true;
+
+	    if (this._dayWidget.getValue().length === 0) {
+	        ok = false;
+	    }
+
+	    if (this._monthWidget.getValue().length === 0) {
+	        ok = false;
+	    }
+
+	    if (this._yearWidget.getValue().length === 0) {
+	        ok = false;
+	    }
+
+	    if (ok === true) {
+            var day   = this._dayWidget.getValue()[0];
+            var month = this._monthWidget.getValue()[0];
+            var year  = this._yearWidget.getValue()[0];
+
+            return (year + '-' + month + '-' + day);
+        } else {
+            return null;
+        }
+	},
+
+	setValue: function(newDate)
+	{
+	    var dateParts = newDate.split(/-/);
+	    var oldCE = this.changeEnabled;
+
+	    this.changeEnabled = false;
+
+	    this._yearWidget.setValue([dateParts[0].toString()]);
+	    this._monthWidget.setValue([dateParts[1].toString()]);
+	    this._dayWidget.setValue([dateParts[2].toString()]);
+
+	    // Now fire the event in one go.
+	    this.changeEnabled = oldCE;
+	    this.dateChanged();
+	},
+
+	getDateObject: function()
+	{
+	    return new Date(this.getValue());
 	},
 };
